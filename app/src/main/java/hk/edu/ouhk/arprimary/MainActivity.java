e5e97f7549573b8075dba572ee55c3beaad3267f
paul <ngwaipang381@gmail.com>
2021-12-07 19:56:12 +0800

    login template created

--- app/src/main/java/hk/edu/ouhk/arprimary/MainActivity.java
+++ app/src/main/java/hk/edu/ouhk/arprimary/MainActivity.java
@@ -1,5 +1,6 @@
 package hk.edu.ouhk.arprimary;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -28,6 +29,10 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+
+                Intent splashIntent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(splashIntent);
+                finish();
             }
         });
     }
