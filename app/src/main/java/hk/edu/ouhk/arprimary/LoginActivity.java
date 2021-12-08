e8e1e27af8006fcad33d9abe2266d5247f97540b
paul <ngwaipang381@gmail.com>
2021-12-08 10:37:01 +0800

    branch created

--- app/src/main/java/hk/edu/ouhk/arprimary/LoginActivity.java
+++ app/src/main/java/hk/edu/ouhk/arprimary/LoginActivity.java
@@ -27,7 +27,7 @@ public class LoginActivity extends AppCompatActivity {
 
 
 
-        
+
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
