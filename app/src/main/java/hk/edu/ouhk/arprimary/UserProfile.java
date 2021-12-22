0e8aab6fc2918b3f0cef94fd661347ccf29d2aac
Ken <suenken1225@gmail.com>
2021-12-23 02:35:31 +0800

    test

--- app/src/main/java/hk/edu/ouhk/arprimary/UserProfile.java
+++ app/src/main/java/hk/edu/ouhk/arprimary/UserProfile.java
@@ -1,35 +1,27 @@
 package hk.edu.ouhk.arprimary;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 
-public class SignUpActivity extends AppCompatActivity {
-    EditText sigName;
-    Button signUp;
-
+public class UserProfile extends AppCompatActivity {
+    Button back;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sign_up);
+        setContentView(R.layout.activity_user_profile);
+        back = findViewById(R.id.Back);
 
-        sigName = findViewById(R.id.sigName);
-        signUp = findViewById(R.id.signUp);
-
-        signUp.setOnClickListener(new View.OnClickListener() {
+        back.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent signUpIntent = new Intent(SignUpActivity.this, HomeActivity.class);
+                Intent signUpIntent = new Intent(UserProfile.this, HomeActivity.class);
                 startActivity(signUpIntent);
                 finish();
             }
         });
-
-
     }
 }
