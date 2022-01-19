63d380ee5bd249273b0b860fe4c453f2d5926c4c
paul <ngwaipang381@gmail.com>
2022-01-19 23:51:04 +0800

    speaker button updated

--- app/src/main/java/hk/edu/ouhk/arprimary/HomeActivity.java
+++ app/src/main/java/hk/edu/ouhk/arprimary/HomeActivity.java
@@ -16,7 +16,7 @@ public class HomeActivity extends AppCompatActivity {
     Button leaderboardBtn;
     Button speachBtn;
 
-    
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
