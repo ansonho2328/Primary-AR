e84a7d90b19ca079059d388b4a07c96e0249c6de
paul <ngwaipang381@gmail.com>
2022-01-19 15:35:55 +0800

    Ar model name added

--- app/src/main/java/hk/edu/ouhk/arprimary/HomeActivity.java
+++ app/src/main/java/hk/edu/ouhk/arprimary/HomeActivity.java
@@ -14,8 +14,7 @@ public class HomeActivity extends AppCompatActivity {
     Button journalBtn;
     Button multiplayerBtn;
     Button leaderboardBtn;
-
-
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
