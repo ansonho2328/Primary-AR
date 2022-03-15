a3238ccd284a226125a0d90364a6146f00721248
paul <ngwaipang381@gmail.com>
2022-03-15 22:58:14 +0800

    Some contents are added to sentenceActivity

--- app/src/main/java/hk/edu/ouhk/arprimary/SentenceActivity.java
+++ app/src/main/java/hk/edu/ouhk/arprimary/SentenceActivity.java
@@ -1,14 +1,61 @@
 package hk.edu.ouhk.arprimary;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.widget.ImageButton;
 
 public class SentenceActivity extends AppCompatActivity {
+
+    ImageButton tipsBtn, leave;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sentence);
+
+        tipsBtn = findViewById(R.id.tipsBtn);
+        leave = findViewById(R.id.leave);
+
+        // Leave button handling for leaving the game
+        leave.setOnClickListener(view -> {
+            AlertDialog.Builder leave = new AlertDialog.Builder(SentenceActivity.this);
+            leave.setIcon(ContextCompat.getDrawable(SentenceActivity.this, R.drawable.confirm_leave));
+            leave.setTitle("Leave Game");
+            leave.setMessage("Are you sure to leave the game?");
+
+            leave.setNegativeButton("Yes", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface arg0, int arg1) {
+                    setResult(RESULT_CANCELED);
+                    finish();
+                    overridePendingTransition(R.anim.slide_in_left, R.anim.slide_out_right);
+                }
+            });
+
+            leave.setPositiveButton("No", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface arg0, int arg1) {
+                }
+            });
+            leave.show();
+        });
+
+        // Tips button handling for game tips or rules
+        tipsBtn.setOnClickListener(view -> {
+            AlertDialog.Builder tips = new AlertDialog.Builder(SentenceActivity.this);
+            tips.setIcon(ContextCompat.getDrawable(SentenceActivity.this, R.drawable.tips));
+            tips.setTitle("Sentence-Making Game Tips");
+            tips.setMessage("1. ");
+            tips.setPositiveButton("Got It", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface arg0, int arg1) {
+                }
+            });
+            tips.show();
+        });
     }
 }
