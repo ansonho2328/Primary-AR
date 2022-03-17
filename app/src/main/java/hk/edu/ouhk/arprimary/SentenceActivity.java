1f96930132f2d9a7c3b61560839ec8702f3140de
paul <ngwaipang381@gmail.com>
2022-03-17 23:53:18 +0800

    commit

---
+++ app/src/main/java/hk/edu/ouhk/arprimary/SentenceActivity.java
@@ -0,0 +1,63 @@
+package hk.edu.ouhk.arprimary;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.widget.ImageButton;
+
+public class SentenceActivity extends AppCompatActivity {
+
+    ImageButton tipsBtn, leave;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sentence);
+
+        tipsBtn = findViewById(R.id.tipsBtn);
+        leave = findViewById(R.id.leave);
+
+        // Leave button handling for leaving the game
+        leave.setOnClickListener(view -> {
+            AlertDialog.Builder leave = new AlertDialog.Builder(SentenceActivity.this);
+            leave.setIcon(ContextCompat.getDrawable(SentenceActivity.this, R.drawable.confirm_leave));
+            leave.setTitle("Leave Game");
+            leave.setMessage("Are you sure to leave the game?");
+
+            leave.setNegativeButton("Yes", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface arg0, int arg1) {
+                    setResult(RESULT_CANCELED);
+                    finish();
+                    overridePendingTransition(R.anim.slide_in_left, R.anim.slide_out_right);
+                }
+            });
+
+            leave.setPositiveButton("No", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface arg0, int arg1) {
+                }
+            });
+            leave.show();
+        });
+
+        // Tips button handling for game tips or rules
+        tipsBtn.setOnClickListener(view -> {
+            AlertDialog.Builder tips = new AlertDialog.Builder(SentenceActivity.this);
+            tips.setIcon(ContextCompat.getDrawable(SentenceActivity.this, R.drawable.tips));
+            tips.setTitle("Sentence-Making Game Tips");
+            tips.setMessage("Present Tense - Subject(S) + Verb(V)/be: e.g I am happy. For 'I' which is subject and 'am' is verb. \n\n" +
+                    "1. select the word by order to make sentence in present tense.\n\n" +
+                    "2. check the grammar for the sentence. ");
+            tips.setPositiveButton("Got It", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface arg0, int arg1) {
+                }
+            });
+            tips.show();
+        });
+    }
+}
