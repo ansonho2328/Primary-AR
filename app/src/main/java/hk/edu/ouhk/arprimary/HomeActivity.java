275615a5e3bb63532abd85992bf479a4f21a0259
ken <suenken1225@gmail.com>
2022-03-31 22:58:15 +0800

    test db

--- app/src/main/java/hk/edu/ouhk/arprimary/HomeActivity.java
+++ app/src/main/java/hk/edu/ouhk/arprimary/HomeActivity.java
@@ -1,20 +1,35 @@
 package hk.edu.ouhk.arprimary;
 
+import static android.content.ContentValues.TAG;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.Toast;
+
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class HomeActivity extends AppCompatActivity {
 
     Button journalBtn;
     Button multiplayerBtn;
     Button leaderboardBtn;
-    Button speachBtn;
+    Button speachBtn,scorebtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +40,7 @@ public class HomeActivity extends AppCompatActivity {
         multiplayerBtn = findViewById(R.id.start_multiplayer_button);
         leaderboardBtn = findViewById(R.id.leaderboard_button);
 //        speachBtn = findViewById(R.id.text_speach_button);
+        scorebtn = findViewById(R.id.score_button);
 
         journalBtn.setOnClickListener(view -> {
             Intent startIntent = new Intent(this, TopicActivity.class);
@@ -42,6 +58,58 @@ public class HomeActivity extends AppCompatActivity {
             startActivity(startIntent);
             overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
         });
+
+
+        scorebtn.setOnClickListener(v -> {
+            int score = 0;
+            String username = "test";
+            FirebaseDatabase database = FirebaseDatabase.getInstance("https://primary-ar-default-rtdb.asia-southeast1.firebasedatabase.app");
+            DatabaseReference myRef = database.getReference("Leaderboard");
+            ArrayList<User> user = new ArrayList<>();
+            myRef.addChildEventListener(new ChildEventListener() {
+                @Override
+                public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                    User value = dataSnapshot.getValue(User.class);
+                    user.add(value);
+                    User temp = null;
+                    for (User x : user) {
+                        if(x.getUsername().equals(username)){
+                            temp = x;
+                        }
+                    }
+                    if(temp != null){
+                        temp.setScore(temp.getScore()+10);
+                        Map<String,Object> update = new HashMap<>();
+                        update.put(username,temp);
+                        myRef.updateChildren(update);
+                    }else{
+                        myRef.child(username).setValue(new User(username,10));
+                    }
+                }
+
+                @Override
+                public void onChildChanged(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+                }
+
+                @Override
+                public void onChildRemoved(@NonNull DataSnapshot snapshot) {
+
+                }
+
+                @Override
+                public void onChildMoved(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+                }
+
+                @Override
+                public void onCancelled(DatabaseError error) {
+                    // Failed to read value
+                    Log.w(TAG, "Failed to read value.", error.toException());
+                }
+            });
+        });
+
 
 //        speachBtn.setOnClickListener(v ->{
 //            Intent startIntent = new Intent(this, SpeechToTextActivity.class);
