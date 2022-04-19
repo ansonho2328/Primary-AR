a9bf2bda24c2495c0fec3bf1099869bf04cdf55a
paul <ngwaipang381@gmail.com>
2022-04-19 23:07:30 +0800

    added back some code to auth activity

--- app/src/main/java/hk/edu/ouhk/arprimary/AuthenticateActivity.java
+++ app/src/main/java/hk/edu/ouhk/arprimary/AuthenticateActivity.java
@@ -29,7 +29,7 @@ public class AuthenticateActivity extends AppCompatActivity {
 
     FirebaseAuth auth;
 
-    
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
