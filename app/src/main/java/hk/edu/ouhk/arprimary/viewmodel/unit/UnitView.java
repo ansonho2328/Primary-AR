841ecfe5835b4a62bf7bab40190a6fddceb894d2
paul <ngwaipang381@gmail.com>
2022-04-20 00:13:40 +0800

    updated UnitView and PlayedHistory

--- app/src/main/java/hk/edu/ouhk/arprimary/viewmodel/unit/UnitView.java
+++ app/src/main/java/hk/edu/ouhk/arprimary/viewmodel/unit/UnitView.java
@@ -2,11 +2,23 @@ package hk.edu.ouhk.arprimary.viewmodel.unit;
 
 public class UnitView {
 
-    private final int no;
-    private final Type type;
+    private int no;
+    private Type type;
+
+    public UnitView() {
+        this.no = 0;
+    }
 
     public UnitView(int no, Type type) {
         this.no = no;
+        this.type = type;
+    }
+
+    public void setNo(int no) {
+        this.no = no;
+    }
+
+    public void setType(Type type) {
         this.type = type;
     }
 
