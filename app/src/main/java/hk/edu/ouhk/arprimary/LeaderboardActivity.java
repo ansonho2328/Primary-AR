d38052acf5e8f63a34b64c247f8f99e7699579d7
paul <ngwaipang381@gmail.com>
2022-04-21 00:23:27 +0800

    added signout button and updated leaderboard and profile UI

--- app/src/main/java/hk/edu/ouhk/arprimary/LeaderboardActivity.java
+++ app/src/main/java/hk/edu/ouhk/arprimary/LeaderboardActivity.java
@@ -27,6 +27,7 @@ public class LeaderboardActivity extends AppCompatActivity {
     ArrayList<Board> boards = new ArrayList<>();
     ArrayAdapter<Board> arrayAdapter;
 
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
