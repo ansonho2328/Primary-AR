bfbf0d923b8a3cc818eefd9cdcbd17d1981600e3
eric2788 <tsukiko852@gmail.com>
2022-04-22 17:29:32 +0800

    added review

--- app/src/main/java/hk/edu/ouhk/arprimary/viewmodel/unit/UnitAdapter.java
+++ app/src/main/java/hk/edu/ouhk/arprimary/viewmodel/unit/UnitAdapter.java
@@ -16,15 +16,9 @@ import hk.edu.ouhk.arprimary.viewmodel.ListExtendableAdapter;
 
 public class UnitAdapter extends ListExtendableAdapter<UnitView, UnitAdapter.ViewHolder> {
 
-    private final Predicate<UnitView> disableIf;
-
-    public UnitAdapter(List<UnitView> list, Context context, View.OnClickListener onClickListener, Predicate<UnitView> disableIf) {
-        super(list, context, onClickListener);
-        this.disableIf = disableIf;
-    }
 
     public UnitAdapter(List<UnitView> list, Context context, View.OnClickListener onClickListener){
-        this(list, context, onClickListener, v -> false);
+        super(list, context, onClickListener);
     }
 
     @Override
@@ -42,7 +36,7 @@ public class UnitAdapter extends ListExtendableAdapter<UnitView, UnitAdapter.Vie
         UnitView view = list.get(position);
         String title = view.getType() == UnitView.Type.PRACTICE ? "Unit" : "Quiz";
         String txt = MessageFormat.format("{0}-{1}", title, view.getNo());
-        if (disableIf.test(view)){
+        if (view.isReview()){
             holder.title.setText(txt + "(completed)");
         }else{
             holder.title.setText(txt);
