d8a60cb5a500da426a57a8db5f77f9fbe8b4b245
eric2788 <tsukiko852@gmail.com>
2022-04-28 21:47:44 +0800

    fix no arg constructor

--- app/src/main/java/hk/edu/ouhk/arprimary/firestore/PlayedHistory.java
+++ app/src/main/java/hk/edu/ouhk/arprimary/firestore/PlayedHistory.java
@@ -9,15 +9,34 @@ import hk.edu.ouhk.arprimary.viewmodel.unit.UnitView;
 
 public class PlayedHistory {
 
-    private final UnitView.Type type;
-    private final int unit;
-    private final int scores;
-    private final String topic;
+    private UnitView.Type type;
+    private int unit;
+    private int scores;
+    private String topic;
+
+    public PlayedHistory() {
+    }
 
     public PlayedHistory(UnitView.Type type, int unit, int scores, String topic) {
         this.type = type;
         this.unit = unit;
         this.scores = scores;
+        this.topic = topic;
+    }
+
+    public void setType(UnitView.Type type) {
+        this.type = type;
+    }
+
+    public void setUnit(int unit) {
+        this.unit = unit;
+    }
+
+    public void setScores(int scores) {
+        this.scores = scores;
+    }
+
+    public void setTopic(String topic) {
         this.topic = topic;
     }
 
