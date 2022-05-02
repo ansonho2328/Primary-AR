fea3b354a0bfa943a8e924bfc6ada36a3872617a
eric2788 <tsukiko852@gmail.com>
2022-05-02 17:44:53 +0800

    fix review activity

--- app/src/main/java/hk/edu/ouhk/arprimary/ReviewActivity.java
+++ app/src/main/java/hk/edu/ouhk/arprimary/ReviewActivity.java
@@ -1,32 +1,42 @@
 package hk.edu.ouhk.arprimary;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 
+import java.text.MessageFormat;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 import hk.edu.ouhk.arprimary.firestore.PlayedHistory;
 import hk.edu.ouhk.arprimary.firestore.User;
+import hk.edu.ouhk.arprimary.manager.ApplicationComponent;
 import hk.edu.ouhk.arprimary.manager.FragmentManager;
-import hk.edu.ouhk.arprimary.model.Lesson;
+import hk.edu.ouhk.arprimary.manager.LessonFragmentManager;
+import hk.edu.ouhk.arprimary.manager.QuizFragmentManager;
+import hk.edu.ouhk.arprimary.manager.SentenceFragmentManager;
 import hk.edu.ouhk.arprimary.model.LessonFragment;
-import hk.edu.ouhk.arprimary.viewmodel.armodel.practice.UnitSection;
+import hk.edu.ouhk.arprimary.model.SentenceFragment;
+import hk.edu.ouhk.arprimary.viewmodel.unit.UnitView;
 
 public class ReviewActivity extends AppCompatActivity {
     CollectionReference historyRef;
     FirebaseUser session;
-    FragmentManager fm;
+
+
+    LessonFragmentManager lessonFragmentManager;
+    SentenceFragmentManager sentencefragmentManager;
+    QuizFragmentManager quizFragmentManager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,6 +44,19 @@ public class ReviewActivity extends AppCompatActivity {
         FirebaseFirestore store = FirebaseFirestore.getInstance();
         FirebaseAuth auth = FirebaseAuth.getInstance();
         this.session = auth.getCurrentUser();
+
+        if (this.session == null){
+            Toast.makeText(this, "Not Login", Toast.LENGTH_LONG).show();
+            //TODO maybe jump tp AuthenticateActivity?
+            return;
+        }
+
+        ApplicationComponent component = ((PrimaryARApplication)getApplicationContext()).appComponent;
+
+        this.quizFragmentManager = component.quizFragmentManager();
+        this.lessonFragmentManager = component.lessonFragmentManager();
+        this.sentencefragmentManager = component.sentenceFragmentManager();
+
         historyRef = store.collection("histories");
         ListView listReview = (ListView) findViewById(R.id.list_review);
         historyRef.document(session.getDisplayName()).get().continueWithTask(task -> {
@@ -41,31 +64,42 @@ public class ReviewActivity extends AppCompatActivity {
                     .ofNullable(task.getResult())
                     .map(r -> r.toObject(User.class))
                     .orElseGet(User::new);
+
+
             List<PlayedHistory> playedHistory = user.getHistories();
-            ArrayList<String> topic = new ArrayList<String>();
-            ArrayList<String> str = new ArrayList<String>();
+            ArrayList<String> show = new ArrayList<String>();
+
             playedHistory.forEach((e) -> {
-                topic.add(e.getTopic());
-            });
-            topic.forEach((t) -> {
-                Optional<LessonFragment[]> lessonFragments = fm.getFragmentsByTopicUnit(t,1);
-                LessonFragment[] fragments = lessonFragments.get();
-                for(int i =0;i < fragments.length;i++){
-                    str.add(fragments[i].getModelName());
+
+                if (e.getType() == UnitView.Type.PRACTICE) {
+
+                    Optional<LessonFragment[]> lessonFragments = lessonFragmentManager.getFragmentsByTopicUnit(e.getTopic(), e.getUnit());
+                    Optional<SentenceFragment[]> sentenceFragments = sentencefragmentManager.getFragmentsByTopicUnit(e.getTopic(), e.getUnit());
+
+                    if (lessonFragments.isPresent()) {
+                        LessonFragment[] fragments = lessonFragments.get();
+                        show.add(MessageFormat.format("Topic: {0} ({1}", e.getTopic(), "Vocab"));
+                        for (LessonFragment fragment : fragments) {
+                            show.add(MessageFormat.format("{0}: {1}", fragment.getModelName(), fragment.getDefinition()));
+                        }
+                    }else if (sentenceFragments.isPresent()){
+                        SentenceFragment[] fragments = sentenceFragments.get();
+                        show.add(MessageFormat.format("Topic: {0} ({1}", e.getTopic(), "Sentence"));
+                        for (SentenceFragment fragment : fragments) {
+                            show.add(fragment.getCorrectAnswer());
+                        }
+                    } else {
+                        Toast.makeText(this, MessageFormat.format("Topic {0}-{1}-{2} does not exist.", e.getTopic(), e.getType(), e.getUnit()), Toast.LENGTH_LONG).show();
+                    }
+
                 }
             });
-            ArrayAdapter adapter = new ArrayAdapter(this,
-                    android.R.layout.simple_list_item_1,
-                    str);
+            ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, show);
             listReview.setAdapter(adapter);
             return null;
         }).addOnCompleteListener(task -> {
-            if (task.isSuccessful()){
-
-            }else{
-                if (task.getException() != null){
-                    task.getException().printStackTrace();
-                }
+            if (!task.isSuccessful() && task.getException() != null) {
+                task.getException().printStackTrace();
             }
         });
     }
